e368f3541e2e4df524bab0e5f0cb45dcdeda5755
li.hui <li.hui@sincetimes.com>
2022-01-12 11:28:29 +0800

    init commit

---
+++ Source/InterpAlgorithm/InterpAlgorithmGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InterpAlgorithmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERPALGORITHM_API AInterpAlgorithmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
